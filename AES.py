034eca586f78854f50c8f2e766e411adae817b26
pdangal <prajjwal.dangal@gmail.com>
2020-03-31 16:15:01 -0600

    table like 6.4 from book working

--- AES.py
+++ AES.py
@@ -3,8 +3,11 @@
 
 # In[128]:
 
-KEY = '0f1571c947d9e8591cb7add6af7f6798'
-# KEY = '0f1571c947d9e8590cb7add6af7f6798'
+from functools import reduce
+import math
+
+# KEY = '0f1571c947d9e8591cb7add6af7f6798' # this is what the question asks
+KEY = '0f1571c947d9e8590cb7add6af7f6798'
 KEY_LENGTH = len(KEY) // 2 # 16 bytes
 
 
@@ -41,6 +44,11 @@ class AES:
         self.nor = r
         
     def sboxSubstitution(self, pltxt):
+        """
+
+        :param pltxt:   the plaintext of type str
+        :return:        a 4x4 matrix of sbox string values
+        """
         pltxt_indices = [[pltxt[i], pltxt[i+1]] for i in range(0,32,2)]
         l = len(pltxt_indices)
         opt = []
@@ -48,10 +56,12 @@ class AES:
             # since Sbox is a flat list, we will index it using just one index
             # which is row * 16 + column
             sbox_ind = int(pltxt_indices[i][0], 16) * 16 + int(pltxt_indices[i][1], 16)
-            opt.append(hex(AES.sBox[sbox_ind]))
+            opt.append(''.join(hex(AES.sBox[sbox_ind]).split("0x")[1:]))
+
         mat = []
         tmp = []
         for i in range(1, 17):
+            # print("type(opt[i-1]): ", type(opt[i-1]))
             tmp.append(opt[i-1])
             if i % 4 == 0:
                 mat.append(tmp)
@@ -59,6 +69,12 @@ class AES:
         return mat
     
     def shiftRows(self, mat):
+        """
+
+        :param mat:     a 4 x 4 matrix representing bytes at each index
+        :return:        a 4 x 4 matrix of strings where the 1st, 2nd, 3rd, and 4th rows are shifted by
+                                    0, 1, 2, and 3 respectively
+        """
         tmp = [mat[1][1], mat[1][2], mat[1][3], mat[1][0]]
         mat[1] = tmp
         tmp = [mat[2][2], mat[2][3], mat[2][0], mat[2][1]]
@@ -76,6 +92,7 @@ class AES:
             returns     the result of multiplying in GF(2^8)
         """
         p = 0
+        b = int(b, 16)
         # loop run for 8 times, representing bits in a byte
         for c in range(8):
             if b & 1:
@@ -104,7 +121,7 @@ class AES:
         """
             inp:    is the 4x4 byte input
 
-            returns   a 4x4 matrix after mixing column
+            returns   a 4x4 matrix of strings after mixing column
         """
         # this function makes use of helper function, multiplyAndXor
         # the code is straight forward
@@ -123,20 +140,47 @@ class AES:
             while j < 4:
                 # grabbing a column
                 inp_mat = [inp[0][j], inp[1][j], inp[2][j], inp[3][j]]
-                tmp.append(AES.multiplyAndXor(multiplier, inp_mat))
+                tmp.append(''.join(hex(AES.multiplyAndXor(multiplier, inp_mat)).split("0x")[1:]).zfill(2))
                 j+=1
             out.append(tmp)
             i+= 1
         return out
 
+    def addRoundKeys(self, mat, key):
+        """
+
+        :param mat:     the output after mix column or shift rows
+        :param key:     the round key
+        :return:        a matrix of numbers after xor-ing mat with key
+        """
+        i=j=0
+        keyT = [[int(key[j][i], 16) for j in range(4)] for i in range(4)]
+        retMat = [[0]*4 for i3 in range(4)]
+
+        for i2 in range(4):
+            for j2 in range(4):
+                # print("i2: {}, j2: {}\nmat: {}\ntype(mat[i2][j2]): {}\ntype(keyT[i2][j2]): {}".format(i2,
+                #                                j2, mat, type(mat[i2][j2]), type(keyT[i2][j2])))
+                retMat[i2][j2] = int(mat[i2][j2], 16) ^ keyT[i2][j2]
+        return retMat
+
+    # keyExpansion algorithm is in line 188
     @staticmethod
     def rotWord(word):
-        # print("word: ", word)
-        #word = hex(word)
+        """
+
+        :param word:    a string that whose length is 8 representing four bytes
+        :return:        a rotated version of the word
+        """
         return word[2:] + word[:2]
 
     @staticmethod
     def subWord(word):
+        """
+
+        :param word:    a string whose length is 8, representing four bytes
+        :return:        a string whose length is 8, representing four bytes, after sbox substitution
+        """
         r1 = word[0]
         c1 = word[1]
         r2 = word[2]
@@ -166,12 +210,14 @@ class AES:
         if len(byte4) <2:
             byte4 = '0' + byte4
         return byte1+byte2+byte3+byte4
-    
-    def addRoundKeys(self):
-        pass
 
     @staticmethod
     def numToHexString(n):
+        """
+
+        :param n:   just a helper function to convert to hex as hex() return a string
+        :return:    the hex part only, no 0x
+        """
 
         if not isinstance(n, int):
             n = int(n, 16)
@@ -181,18 +227,38 @@ class AES:
 
     @staticmethod
     def chunkWord(strWord):
+        """
+
+        :param strWord:     a string whose length is 8
+        :return:            a string whose length is 8 + three additional spaces for better illustration
+        """
         return strWord[:2] + " " + strWord[2:4] + " " + strWord[4:6] + " " + strWord[6:8]
 
     # @property
     def keyExpansion(self):
+        """
+        Expands the given key of an AES algorithm and prints the process as a table.
+        :return:    the keys for each round, 10 in this case
+        """
 
         def getX(word):
+            """
+
+            :param word:    a string whose length is 8, representing 4 bytes
+            :return:
+            """
             return AES.rotWord(word)
 
         def getY(rotated):
             return AES.subWord(rotated)
 
         def getZ(subStr, i):
+            """
+
+            :param subStr:  the 8 characters long string obtained after sbox operation
+            :param i:       the index into the rcon list, also represents the round number
+            :return:        a string after xoring the first byte from subStr with rcon[round_number]
+            """
             rcon = AES.rCon[i // 4]
             tmp = int(subStr[:2], 16)  ^ rcon
             tmp = AES.numToHexString(tmp)
@@ -203,7 +269,15 @@ class AES:
 
 
         def printTable(ws, xs, ys, zs):
-            import math
+            """
+
+            :param ws:      the list of round keys, length is 10
+            :param xs:      the list of rotated words, length is 10
+            :param ys:      the list of sbox substituted words after rotation, length is 10
+            :param zs:      the list of y ^ r, length is 10
+            :return:        -
+            """
+
             print("w{} = {}\t\t\t\t\t| RotWord (w{}) = {} = x{}".format(0, AES.chunkWord(ws[0]),
                 3,  AES.chunkWord(xs[0]), 1, end=" "))
             print("w{} = {}\t\t\t\t\t| SubWord (x{}) = {} = y{}".format(1, AES.chunkWord(ws[1]),
@@ -227,7 +301,7 @@ class AES:
                     # column 2
                     print("SubWord (x{}) = {} = y{}".format(i//4+1, AES.chunkWord(ys[i//4]), i//4+1))
                 elif i % 4 == 2:
-                    print("w{} = w{} xor w{} = {}\t\t|".format(i, i-4, i-4, AES.chunkWord(ws[i])), end=" ")
+                    print("w{} = w{} xor w{} = {}\t\t|".format(i, i-1, i-4, AES.chunkWord(ws[i])), end=" ")
                     # column 2
                     print("Rcon ({}) = {} 00 00 00".format(math.ceil(i/4), hex(AES.rCon[math.ceil(i / 4)]).split("0x")[1], end=" "))
                 else:
@@ -235,15 +309,14 @@ class AES:
                     # column 2
                     print("y{} xor Rcon ({}) = {}".format(i//4+1, i//4+1, AES.chunkWord(zs[i//4])))
             print("-------------------------------------------------------------------------------")
-            print("w{} = w{} xor z{} = {}".format(40, 36, 10, AES.chunkWord(ws[40])))
-            print("w{} = w{} xor z{} = {}".format(41, 40, 37, AES.chunkWord(ws[41])))
-            print("w{} = w{} xor z{} = {}".format(42, 41, 38, AES.chunkWord(ws[42])))
-            print("w{} = w{} xor z{} = {}".format(43, 42, 39, AES.chunkWord(ws[43])))
+            print("w{} = w{} xor w{} = {}".format(40, 36, 10, AES.chunkWord(ws[40])))
+            print("w{} = w{} xor w{} = {}".format(41, 40, 37, AES.chunkWord(ws[41])))
+            print("w{} = w{} xor w{} = {}".format(42, 41, 38, AES.chunkWord(ws[42])))
+            print("w{} = w{} xor w{} = {}".format(43, 42, 39, AES.chunkWord(ws[43])))
         w = ['' for i in range(44)]
         l = len(KEY)
         for i in range(4):
-            strWord = KEY[8*i: 8*i+8]
-            w[i] = strWord
+            w[i] = KEY[8*i: 8*i+8]
         
         xs = []
         ys = []
@@ -261,28 +334,189 @@ class AES:
                 z = getZ(y, i)
                 zs.append(z)
                 temp = z
-            w[i] = AES.numToHexString(hex(int(w[i-4], 16) ^ int(temp, 16)))
+            w[i] = ''.join(hex(int(w[i-4], 16) ^ int(temp, 16)).split("0x")[1:]).zfill(8)
         printTable(w, xs, ys, zs)
         return w
 
-def format_output(msg, data):
-    print("\033[1;30;0m {}\n\t".format(msg))
+def format_output(msg, data, hx=False):
+
+    print("{}\t".format(msg))
     if data:
-        print("\033[1;30;47m{}".format(data))
+        if isinstance(data, list):
+            for i in range(4):
+                print("\033[1;30;0m {}".format(data[i]))
+
+        else:
+            print("\033[1;30;0m {}".format(data))
+
+
+def printTable(inp, subBytes, shifted, mc, roundKeyT):
+    # <class 'list'>: [['0xab', '0x43', '0x3b', '0xe8'], ['0xae', '0xb2', '0x48', '0x53'], ['0xd0', '0xc4', '0x41', '0x1a'], ['0x1b', '0xf2', '0x4e', '0x34']]
+    # let's transpose
+    def transpose(mat):
+        # if not mat:
+        #     return []
+        tmp = [["a"]*4 for i in range(4)]
+        for i in range(4):
+            for j in range(4):
+                tmp[i][j] = subBytes[j][i]
+        inp = transpose(inp)
+        subBytes = transpose(subBytes)
+        shifted  = transpose(shifted)
+        mc = transpose(mc)
+
+    tmp = [["a"] * 4 for i2 in range(4)]
+    for i in range(4):
+        for j in range(4):
+            tmp[i][j] = ''.join(hex(roundKeyT[i][j]).split("0x")[1:])
+    roundKeyT = tmp
+
+    if subBytes and shifted:
+        # check if need to transpose matrices
+        if mc[0] == shifted[0][1:] and mc[1] == shifted[1][1:]:
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[0]),
+                                                                 ' '.join(subBytes[0]),
+                                                                ' '.join(shifted[0]),
+                                                                 "",
+                                                                ' '.join(roundKeyT[0])))
+
+
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[1]),
+                                                                 ' '.join(subBytes[1]),
+                                                                ' '.join(shifted[1]),
+                                                                 "",
+                                                                ' '.join(roundKeyT[1])))
+
+
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[2]),
+                                                                 ' '.join(subBytes[2]),
+                                                                ' '.join(shifted[2]),
+                                                                 "",
+                                                                ' '.join(roundKeyT[2])))
+
+
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[3]),
+                                                                 ' '.join(subBytes[3]),
+                                                                ' '.join(shifted[3]),
+                                                                 " ",
+                                                                ' '.join(roundKeyT[3])))
+        else:
+            # print("Need to transpose subBytes, shifted, mc, maybe inp")
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[0]),
+                                                                 ' '.join(subBytes[0]),
+                                                                 ' '.join(shifted[0]),
+                                                                 ' '.join(mc[0]),
+                                                                 ' '.join(roundKeyT[0])))
+
+
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[1]),
+                                                                 ' '.join(subBytes[1]),
+                                                                 ' '.join(shifted[1]),
+                                                                 ' '.join(mc[1]),
+                                                                 ' '.join(roundKeyT[1])))
+
+
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[2]),
+                                                                 ' '.join(subBytes[2]),
+                                                                 ' '.join(shifted[2]),
+                                                                 ' '.join(mc[2]),
+                                                                 ' '.join(roundKeyT[2])))
+
+
+            print("{}\t\t|  {}\t\t|   {}\t\t|   {}\t\t|    {}\t|".format(' '.join(inp[3]),
+                                                                 ' '.join(subBytes[3]),
+                                                                 ' '.join(shifted[3]),
+                                                                 ' '.join(mc[3]),
+                                                                 ' '.join(roundKeyT[3])))
+    else:
+        print("{}\t\t|\t\t\t\t\t|\t\t\t\t\t|\t\t\t\t\t|    {}\t\t|".format(' '.join(inp[0]), ' '.join(roundKeyT[0])))
+        print("{}\t\t|\t\t\t\t\t|\t\t\t\t\t|\t\t\t\t\t|    {}\t\t|".format(' '.join(inp[1]), ' '.join(roundKeyT[1])))
+        print("{}\t\t|\t\t\t\t\t|\t\t\t\t\t|\t\t\t\t\t|    {}\t\t|".format(' '.join(inp[2]), ' '.join(roundKeyT[2])))
+        print("{}\t\t|\t\t\t\t\t|\t\t\t\t\t|\t\t\t\t\t|    {}\t\t|".format(' '.join(inp[3]), ' '.join(roundKeyT[3])))
+
+
+        pass
+    print("-------------------------------------------------------------------------------------------------------")
+    return
 
 if __name__ == '__main__':
+
+    # initialize
     cipher = AES(KEY_LENGTH, NOR)
     pltxt = '0123456789abcdeffedcba9876543210'
     format_output("Plaintext: ", ' '.join(pltxt))
-    opt_sbox = cipher.sboxSubstitution(pltxt)
-    format_output("Applying sbox operation on plaintext and returning a " +
-        "matrix for row shifting ...", "")
-    format_output("After sbox operation: ", opt_sbox)
-    format_output("Next shifting rows ...", "")
-    shifted = cipher.shiftRows(opt_sbox)
-    format_output("After row shifts: ", shifted)
-    format_output("Next mixing columns ...", "")
-    # mc = cipher.mixColumns(shifted)
+
+    # key expansion
     print("Expanding key: ", ' '.join(KEY))
-    cipher.keyExpansion()
+    w = cipher.keyExpansion()
+    inp  = pltxt
+    print ("Start of Round\t|  After SubBytes\t|  After ShiftRows\t|  After MixColumns\t|  RoundKey\t\t\t|")
+    for i in range(-1, cipher.nor):
+        # format_output("Round {}".format(i), "")
+        # perform sbox substitution
+
+        # creating a matrix representation of inp_mat but we use the flattened representation in sbox func
+        inpMat = [  [0]  *  4   for  i5   in   range(4)]
+        for r in range(0,4):
+            for c in range(0,7,2):
+                # print("r: {}, c: {}".format(r, c))
+                inpMat[r][c//2] = inp[r * 4 + c] + inp[r * 4 + c + 1]
+        key = w[(i+1)*4:(i+2)*4]
+        # let's convert the string array into array with strings
+        # i.e ['0f1571c9', '47d9e859', '0cb7add6', 'af7f6798'] should be converted to
+        # ==> [['0f', '15', '71', 'c9'], ['47', 'd9', ..]]
+        arr = []
+        # print("key: ", key)
+        for i4 in range(0,4):
+            tmp = []
+            for j4 in range(0,7,2):
+                # print("i4: {}, j4: {}, byte: {}".format(i4, j4, key[i4][j4]+key[i4][j4+1]))
+                tmp.append(key[i4][j4]+key[i4][j4+1])
+            arr.append(tmp)
+        key = arr
+        # case of when the algorithm rounds have not started
+        if i < 0:
+            opt_sbox = []
+            shifted  = []
+            mc = inpMat
+        # round 0 and onwards
+        else:
+
+            # sbox takes a 32 nibble input in the form of string, set it like that so carry-over has to be accounted for
+            opt_sbox = cipher.sboxSubstitution(inp)
+            # shift rows
+            shifted = cipher.shiftRows(opt_sbox)
+            # mix columns
+            if i > cipher.nor - 1:
+                mc = shifted
+
+            else:
+                mc = cipher.mixColumns(shifted)
+        # transposing key which is why the order is key[c2][r2], not row before column
+        roundKey = [[int(key[c2][r2], 16) for c2 in range(4)] for r2 in range(4)]
+        printTable(inpMat, opt_sbox, shifted, mc, roundKey)
+
+        # add round key
+        res1 = cipher.addRoundKeys(mc, key)
+        # print("Length of res1: {}, of res1[0]: {}".format(len(res1), len(res1[0])))
+        res = [[0, 0, 0, 0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+        for i2 in range(4):
+            for j2 in range(4):
+                res[i2][j2] = ''.join(hex(res1[i2][j2]).split("0x")[1:])
+
+        inp = reduce(lambda z, y :z + y, res)
+        # inp is sometimes 31 nibbles long, so have to insert a 0 to a lone character
+        s = ""
+
+        for i3 in inp:
+            if len(i3) < 2:
+                s += "0"+i3
+            else:
+                s += i3
+        inp = s
+        # format_output("Result after round {}".format(i), res, True)
+        # format_output("", "")
+        # print("##########################################################"
+        #      "######################################")
+
 
